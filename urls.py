85f14e118af12eecbb2f56be3387e791ddabdbcb
adikshagole <adikshagole24@gmail.com>
2026-06-25 21:23:48 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,24 +1,28 @@
+"""
+URL configuration for WebProject project.
 
-
-from django.urls import path
-from .views import *
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('menu/', menu, name='menu'),
-    path('about/', about, name='about'),
-    path('customize/', customize, name='customize'),
-    path('order/', order, name='order'),
-    path('add/', add_item, name='add_item'),
-    path('edit/<int:pk>/', edit_item, name='edit_item'),
-    path('delete/<int:pk>/', delete_item, name='delete_item'),
-
-    path('register/', register_page, name='register_page'),
-    path('login/', login_page, name='login_page'),
-    path('logout/', logout_page, name='logout_page'),
-
-    path('cart/', cart, name='cart'),
-    path('add-to-cart/<int:pk>/', add_to_cart, name='add_to_cart'),
-    path('update-quantity/<int:pk>/<str:action>/', update_quantity, name='update_quantity'),
-    path('remove-item/<int:pk>/', remove_item, name='remove_item'),
+    path('admin/', admin.site.urls),
+    path('', include('shop.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
